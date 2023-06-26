3578c78e10c2adb86cfd8ff84a6a56720b9f1772
Boburm1rzo <137428656+Boburm1rzo@users.noreply.github.com>
2023-06-26 21:30:56 +0500

    Добавьте файлы проекта.

---
+++ lesson3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("belgini kirit");
+            char belgi=char.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            int b = int.Parse(Console.ReadLine());
+            if (belgi == '*' )
+            {
+                Console.WriteLine(a + "*" + b + "=" + a * b);
+            } 
+            else if(belgi == '/') 
+            {
+                Console.WriteLine(a + "/" + b + "=" + a / b);
+            }
+            else if (belgi == '-')
+            {
+                Console.WriteLine($"{a} - {b} = {a-b}");
+            }
+            else if (belgi == '+')
+            {
+                Console.WriteLine(a + "+" + b + "=" + a + b);
+            }
+        }
+    }
+}
